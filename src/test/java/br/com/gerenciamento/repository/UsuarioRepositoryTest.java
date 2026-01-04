0be6b04b396f2b04a2087627ae761557244a7b42
Raphael <augustorapha27gmail.com>
2026-01-03 22:32:25 -0300

    test: configuração base dos testes da classe AlunoRepositoryTest; test: teste do método findByStatusAtivo; test: teste do método findByStatusInativo; test: teste do método findAll com repositório vazio; test: teste do método findByNomeContainingIgnoringCase;

--- src/test/java/br/com/gerenciamento/repository/UsuarioRepositoryTest.java
+++ src/test/java/br/com/gerenciamento/repository/UsuarioRepositoryTest.java
@@ -1,10 +1,60 @@
 package br.com.gerenciamento.repository;
 
-import org.junit.runner.RunWith;
+import br.com.gerenciamento.model.Usuario;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 public class UsuarioRepositoryTest {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @BeforeEach
+    void setUp() {
+        usuarioRepository.deleteAll();
+        Usuario usuario = new Usuario();
+        usuario.setId(1L);
+        usuario.setEmail("teste@email.com");
+        usuario.setSenha("senha");
+        usuario.setUser("admin");
+        usuarioRepository.save(usuario);
+    }
+
+    @Test
+    public void testFindByEmail(){
+        Usuario resultado = usuarioRepository.findByEmail("teste@email.com");
+
+        assertNotNull(resultado, "Não pode ser null!");
+        assertEquals("teste@email.com", resultado.getEmail());
+        assertEquals("admin", resultado.getUser());
+    }
+
+    @Test
+    public void testFindByEmail_ExceptionNonExistentEmail(){
+        Usuario resultado = usuarioRepository.findByEmail("inexistente@gmail.com");
+
+        assertNull(resultado, "nulo");
+    }
+
+    @Test
+    public void testBuscarLogin(){
+        Usuario resultado = usuarioRepository.buscarLogin("admin", "senha");
+
+        assertNotNull(resultado, "Não pode ser null!");
+        assertEquals("admin", resultado.getUser());
+        assertEquals("senha", resultado.getSenha());
+    }
+
+    @Test
+    public void testBuscarLogin_ExceptionNonExistentLogin(){
+        Usuario resultado = usuarioRepository.buscarLogin("inexistente", "errada");
+
+        assertNull(resultado, "nulo");
+    }
+
 }
